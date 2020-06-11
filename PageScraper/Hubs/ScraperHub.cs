9a6bb9ca6a77028be46386f058032bcd84057c82
Tobias Gärdebro <tubbeg@gmail.com>
2020-06-11 19:40:40 +0200

    Added signalR to project. Still need to test and configure.

---
+++ PageScraper/Hubs/ScraperHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PageScraper.Hubs
+{
+    public class ScraperHub : Hub
+    {
+        public ScraperHub()
+        {
+
+        }
+    }
+}
